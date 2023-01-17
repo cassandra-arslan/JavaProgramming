5e92ee3f0bbc165ebc8ace39b9b5863e159bebf8
Cassandra Arslan <naimaarslan@gmail.com>
2023-01-16 17:43:24 -0800

    Testing

--- day1/Task1PalindromeNumber.java
+++ day1/Task1PalindromeNumber.java
@@ -10,24 +10,46 @@ public class Task1PalindromeNumber {
         Do not use a String
 
         Ex:
-            Given n = 1234321 -> palindrome
+            Given n = 1234321 -> palindrome   - how to find the last digit mathematically %10
             Given n = 15651 -> palindrome
             Given n = 18374 -> not palindrome
             Given n = 1264628 -> not palindrome
+
+
+            7894566/10=789456 remainder 6  789456/10 remainder 6
+
      */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     public static void main(String[] args) {
 
-        int n = 12321;
+        int n = 1264628;
         int orig = n;
         int reverse = 0;
 
         while(n > 0){
             int lastDigit = n % 10; // reads the last digit in the number
-            reverse = reverse * 10 + lastDigit;
+            reverse = reverse * 10 + lastDigit; //0*10=0+1  -> 1*10=10+2= 12 -> 12*10=120+3=123
             n /= 10;
         }
+        System.out.println(n);
 
         System.out.println(orig + " " + (reverse == orig ? "Palindrome" : "Not Palindrome"));
 
